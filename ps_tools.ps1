b1b6ee02e3321dc89653d28049f44458fee721d0
Cass Golkin <cassgolkin@gmail.com>
2023-04-28 16:18:21 -0400

    Refined it to single entries

--- ps_tools.ps1
+++ ps_tools.ps1
@@ -1,14 +1,18 @@
 Import-Csv .\compNameList.csv | ForEach-Object {
     $objComputer = Get-ADComputer "$($_.Name)"
     
-
-
     $Bitlocker_Object = Get-ADObject -Filter {objectclass -eq 'msFVE-RecoveryInformation'} -SearchBase $objComputer.DistinguishedName -Properties 'msFVE-RecoveryPassword' | Select-Object msFVE-RecoveryPassword
-
 
     "$($_.Name)"
     if ($Bitlocker_Object.count -eq 0) {
         Write-Host "no entry"
+    }elseif ($Bitlocker_Object.count -gt 1) {
+        
+        foreach ($Object in $Bitlocker_Object)
+        {
+            $Final_Object = $Object
+        }
+        $Final_Object
     }else {
         $Bitlocker_Object
     }
