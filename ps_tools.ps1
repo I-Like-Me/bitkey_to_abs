bf7a1fdb1aa47b0c1872d5548b47a274a6ece4ce
Cass Golkin <cassgolkin@gmail.com>
2023-04-28 15:04:16 -0400

    output name and bitkey added

--- ps_tools.ps1
+++ ps_tools.ps1
@@ -1,8 +1,16 @@
 Import-Csv .\compNameList.csv | ForEach-Object {
     $objComputer = Get-ADComputer "$($_.Name)"
+    
 
-    $Bitlocker_Object = Get-ADObject -Filter {objectclass -eq 'msFVE-RecoveryInformation'} -SearchBase $objComputer.DistinguishedName -Properties 'msFVE-RecoveryPassword'
 
-    $Bitlocker_Object
+    $Bitlocker_Object = Get-ADObject -Filter {objectclass -eq 'msFVE-RecoveryInformation'} -SearchBase $objComputer.DistinguishedName -Properties 'msFVE-RecoveryPassword' | Select-Object msFVE-RecoveryPassword
+
+
+    "$($_.Name)"
+    if ($Bitlocker_Object.count -eq 0) {
+        Write-Host "no entry"
+    }else {
+        $Bitlocker_Object
+    }
 }
 
