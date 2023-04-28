5ddb827a5e4023edb59bc61f7bb2a7684c31d8d9
Cass Golkin <cassgolkin@gmail.com>
2023-04-28 11:11:51 -0400

    addressed merge issue

--- driver.py
+++ driver.py
@@ -4,14 +4,7 @@ import subprocess, sys
 
 abs_data = ABS.Abs_Actions.get_all()
 
-<<<<<<< HEAD
 with open('compNameList.csv', 'w') as wf:
-=======
-for device in abs_data['data']:
-    compNames.append(device['deviceName'])
-
-with open('compNameList', 'w') as wf:
->>>>>>> 376d9cb2df51ea7f51b260b8dbab73a59d987a09
     write = csv.writer(wf)
     write.writerow(['Name'])
     for device in abs_data['data']:
@@ -24,7 +17,6 @@ with open('compNameList.csv', newline='') as rf:
         for obj in col:
             print(obj)
 
-<<<<<<< HEAD
 setup_script = 'ps_tools.ps1'
 
 p = subprocess.run([
@@ -33,6 +25,3 @@ p = subprocess.run([
     setup_script,
 ],
 stdout=sys.stdout)
-=======
-print(len(compNames))
->>>>>>> 376d9cb2df51ea7f51b260b8dbab73a59d987a09
